149260c10082e4ad8768b8f80282047182e12f3f
ayuuushhiiii <ayu959822@gmail.com>
2025-12-09 20:41:47 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "server_tf" {
-  ami           = var.ami_id
-  instance_type = var.instance_type_value
-  subnet_id = var.subnet_id
-  tags = merge({
-    Name = var.instance_name
-  }, var.tags)
-}
